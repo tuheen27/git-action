d1730c3522d18c06b50c619765c4d9740830dccd
Tuhin <145905935+tuheen27@users.noreply.github.com>
2025-10-05 13:49:00 +0530

    changes in app.py file

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def home():
     return "Hello, CI/CD with GitHub Actions!"
 
+@app.route('/health')
+def health():
+                return "OK", 200
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
